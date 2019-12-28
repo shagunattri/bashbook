38d16905a30bb532e29159dde59df7c00c8ff64c
ID10t <shagunattri17@gmail.com>
2019-12-28 16:17:27 +0530

    sh files

---
+++ input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#User input
+
+read -p "Enter your Name: " NAME
+echo "Your Name is $NAME"
+
+echo "Oh!! Please enter your full name: "
+read FNAME LNAME
+
+echo "OK! Yoru Full name is: $FNAME $LNAME"
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAME="Shagun"
+SPORT="F1"
+
+
+echo "${NAME}'s favourite sport is $SPORT"
